2095799bd565f7d46144fcac8520177c0acc4ea1
nrv2391 <navemen23@hotmail.com>
2025-09-07 23:41:04 -0600

    inicio de seccion de excepciones

---
+++ BasicOOP/Exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string input ="Hello, World!";
+int number = ParseStringToInt(input); // This will throw a FormatException
+
+
+int ParseStringToInt(string str)
+{
+        return int.Parse(str);
+    
+    
+}
+Console.WriteLine("This line will not be executed due to the exception above.");
