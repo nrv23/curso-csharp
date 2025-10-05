ae5a8d7024475526b60967e736db5dbc38aee3ea
nrv2391 <navemen23@hotmail.com>
2025-10-05 00:36:04 -0600

    inicio de seccion de colecciones

--- BasicOOP/AdvancedTypes/obj/Debug/net7.0/AdvancedTypes.AssemblyInfo.cs
+++ BasicOOP/AdvancedTypes/obj/Debug/net7.0/AdvancedTypes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051f8f108cba1b92b3e0f13ebc7e8d3ce25bc809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6639bbf27b731a6901442d8681a0c3d2e86cff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BasicOOP/Collections/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+using System.Collections;
+
+string text = "Hello, World!";
+
+var customeCollection = new CustomCollection(new string[] { "Hello", "from", "Custom", "Collection" });
+
+foreach (char c in text)
+{
+    Console.WriteLine(c);
+}
+// custoemCollection usando foreach
+Console.WriteLine("\ncustoemCollection usando foreach\n");
+foreach (string c in customeCollection)
+{
+    Console.WriteLine(c);
+}
+
+// como .net implementa IEnumerable
+Console.WriteLine("\nUsando IEnumerator\n");
+IEnumerator textEnumerator = text.GetEnumerator();
+object currentWord;
+while (textEnumerator.MoveNext()) // MoveNext retorna true se houver mais elementos o no ha llegado al final de la lista
+{
+    currentWord = textEnumerator.Current;
+    Console.WriteLine(currentWord);
+}
+
+Console.ReadKey();
+
+class CustomCollection : IEnumerable
+{
+    private string[] items { get; }
+
+    public CustomCollection(string[] items)
+    {
+        this.items = items;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+       return new WordsEnumerator(items);
+    }
+};
+
+class WordsEnumerator : IEnumerator
+{
+    private const int _InitialPosition = -1;
+    private int _currentPosition { get; set; } = _InitialPosition;
+    private readonly string[] words;
+
+    public WordsEnumerator(string[] words)
+    {
+        this.words = words;
+    }
+
+    public object Current
+    {
+        get
+        {
+            try
+            {
+                return words[_currentPosition];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new IndexOutOfRangeException($"{nameof(CustomCollection)}'s has reached", ex);
+            }
+        }
+    }
+
+    public bool MoveNext()
+    {
+        _currentPosition++;
+        return _currentPosition < words.Length;
+    }
+
+    public void Reset()
+    {
+        _currentPosition = _InitialPosition;
+    }
+}
